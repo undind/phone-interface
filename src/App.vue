dfe1aed9d15aefa2409820d563737f1118d59649
Sergey Antonov <flyker777@yandex.ru>
2019-11-25 16:43:31 +0300

    start project

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template lang="pug">
+  h1 Welcome to the Vue App
+</template>
+
+<style lang="postcss">
+@import url('https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800');
+@import "normalize.css";
+@import "./styles/mixins.pcss";
+</style>
