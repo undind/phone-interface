93c80db38c1c0729e6b84aec12bd7e534b0af944
undind <flyker777@yandex.ru>
2020-02-29 22:32:13 +0300

    input phone

--- src/views/Dialpad.vue
+++ src/views/Dialpad.vue
@@ -43,34 +43,70 @@
         BasicInput(
           v-model="contact.phone_number"
           size="large"
+          @keypress="isNumber($event)"
         )
       ul.dialpad__buttons
         li.dialpad__btn
           button(
+            value="1"
             @click="press"
           ).dialpad__btn-num 1
         li.dialpad__btn
-          button.dialpad__btn-num 2
+          button(
+            value="2"
+            @click="press"
+          ).dialpad__btn-num 2
         li.dialpad__btn
-          button.dialpad__btn-num 3
+          button(
+            value="3"
+            @click="press"
+          ).dialpad__btn-num 3
         li.dialpad__btn
-          button.dialpad__btn-num 4
+          button(
+            value="4"
+            @click="press"
+          ).dialpad__btn-num 4
         li.dialpad__btn
-          button.dialpad__btn-num 5
+          button(
+            value="5"
+            @click="press"
+          ).dialpad__btn-num 5
         li.dialpad__btn
-          button.dialpad__btn-num 6
+          button(
+            value="6"
+            @click="press"
+          ).dialpad__btn-num 6
         li.dialpad__btn
-          button.dialpad__btn-num 7
+          button(
+            value="7"
+            @click="press"
+          ).dialpad__btn-num 7
         li.dialpad__btn
-          button.dialpad__btn-num 8
+          button(
+            value="8"
+            @click="press"
+          ).dialpad__btn-num 8
         li.dialpad__btn
-          button.dialpad__btn-num 9
+          button(
+            value="9"
+            @click="press"
+          ).dialpad__btn-num 9
         li.dialpad__btn
-          button.dialpad__btn-num *
+          button(
+            value="*"
+            @click="press"
+          ).dialpad__btn-num *
         li.dialpad__btn
-          button.dialpad__btn-num 0
+          button(
+            value="0"
+            @dblclick="press"
+            @click="press"
+          ).dialpad__btn-num 0
         li.dialpad__btn
-          button.dialpad__btn-num #
+          button(
+            value="#"
+            @click="press"
+          ).dialpad__btn-num #
       button(
         @click="createNewStory(contact)"
       ).dialpad__call
@@ -81,203 +117,214 @@
 import { mapActions, mapState } from "vuex";
 
 export default {
-    components: {
-        Icon: () => import("components/Icon.vue"),
-        AppHeader: () => import("components/AppHeader.vue"),
-        BasicInput: () => import("components/BasicInput.vue"),
-        Preloader: () => import("components/Preloader.vue")
+  components: {
+    Icon: () => import("components/Icon.vue"),
+    AppHeader: () => import("components/AppHeader.vue"),
+    BasicInput: () => import("components/BasicInput.vue"),
+    Preloader: () => import("components/Preloader.vue")
+  },
+  data() {
+    return {
+      contact: {
+        type: Object,
+        default: () => ({}),
+        phone_number: ""
+      },
+      isTrue: false,
+      value: "",
+      isLoading: false
+    };
+  },
+  computed: {
+    ...mapState("histories", { histories: state => state.histories })
+  },
+  methods: {
+    ...mapActions("histories", ["createHistory", "fethcHistories"]),
+    goBack(id) {
+      let lastView = localStorage.getItem("LAST_PAGE");
+      if (lastView === "null" && id === undefined) {
+        return this.$router.replace("/");
+      }
+      this.$router.replace({ name: `${lastView}`, params: { id: id } });
     },
-    data() {
-        return {
-            contact: {
-                type: Object,
-                default: () => ({})
-            },
-            isTrue: false,
-            value: "",
-            isLoading: false
-        };
+    createNewContact(contact) {
+      this.$router.replace({ name: "profile", params: { contact: contact } });
     },
-    computed: {
-        ...mapState("histories", { histories: state => state.histories })
-    },
-    methods: {
-        ...mapActions("histories", ["createHistory", "fethcHistories"]),
-        goBack(id) {
-            let lastView = localStorage.getItem("LAST_PAGE");
-            if (lastView === "null" && id === undefined) {
-                return this.$router.replace("/");
-            }
-            this.$router.replace({ name: `${lastView}`, params: { id: id } });
-        },
-        createNewContact(contact) {
-            this.$router.replace({ name: "profile", params: { contact: contact } });
-        },
-        async createNewStory(contact) {
-            const id = contact.contact_id;
-            if (!id) return this.$router.replace({ name: "profile", params: { contact: contact } });
+    async createNewStory(contact) {
+      const id = contact.contact_id;
+      if (!id) return this.$router.replace({ name: "profile", params: { contact: contact } });
 
-            this.isLoading = true;
-            try {
-                await this.createHistory(id);
-            } catch (e) {
-                console.log("Need id");
-                throw new Error(e);
-            } finally {
-                this.isLoading = false;
-            }
-        },
-        async fethcAllHistories() {
-            this.isLoading = true;
-            try {
-                await this.fethcHistories();
-            } catch (e) {
-                throw new Error(e);
-            } finally {
-                this.isLoading = false;
-            }
-        },
-        press(event) {
-            this.value = event.target.innerText;
-        }
+      this.isLoading = true;
+      try {
+        await this.createHistory(id);
+      } catch (e) {
+        console.log("Need id");
+        throw new Error(e);
+      } finally {
+        this.isLoading = false;
+      }
     },
-    async mounted() {
-        await this.fethcAllHistories();
-        if (this.$route.params.contact !== undefined) {
-            this.contact = this.$route.params.contact;
-        }
+    async fethcAllHistories() {
+      this.isLoading = true;
+      try {
+        await this.fethcHistories();
+      } catch (e) {
+        throw new Error(e);
+      } finally {
+        this.isLoading = false;
+      }
+    },
+    press(event) {
+      event.preventDefault()
+      this.contact.phone_number += event.target.value;
+    },
+    isNumber(evt) {
+      evt = evt ? evt : window.event;
+      let charCode = evt.which ? evt.which : evt.keyCode;
+      if (charCode > 31 && (charCode < 48 || charCode > 57) && charCode !== 46) {
+        evt.preventDefault();
+      } else {
+        return true;
+      }
     }
+  },
+  async mounted() {
+    await this.fethcAllHistories();
+    if (this.$route.params.contact !== undefined) {
+      this.contact = this.$route.params.contact;
+    }
+  }
 };
 </script>
 
 <style lang="postcss" scoped>
 .background {
-    width: 425px;
-    height: 850px;
-    margin: 50px auto;
-    border-radius: 65px;
-    overflow: hidden;
-    position: relative;
+  width: 425px;
+  height: 850px;
+  margin: 50px auto;
+  border-radius: 65px;
+  overflow: hidden;
+  position: relative;
 
-    &::after {
-        content: "";
-        position: absolute;
-        background: url("../images/content/background.png") center center / cover no-repeat;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-    }
+  &::after {
+    content: "";
+    position: absolute;
+    background: url("../images/content/background.png") center center / cover no-repeat;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
 }
 
 .header__buttons {
-    position: relative;
-    z-index: 1;
-    padding: 0 38px 15px 46px;
-    display: flex;
-    justify-content: space-between;
+  position: relative;
+  z-index: 1;
+  padding: 0 38px 15px 46px;
+  display: flex;
+  justify-content: space-between;
 }
 
 .header__button-back {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background: none;
-    border: none;
-    outline: none;
-    padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: none;
+  border: none;
+  outline: none;
+  padding: 0;
 }
 
 .header__button-icon {
-    width: 30px;
-    height: 30px;
+  width: 30px;
+  height: 30px;
 }
 
 .header__button-icon--save {
-    width: 40px;
-    height: 40px;
+  width: 40px;
+  height: 40px;
 }
 
 .dialpad {
-    padding: 10px 50px;
-    position: relative;
-    z-index: 1;
-    height: 100%;
+  padding: 10px 50px;
+  position: relative;
+  z-index: 1;
+  height: 100%;
 }
 
 .dialpad__history {
-    margin-bottom: 20px;
-    height: 97px;
-    overflow: auto;
+  margin-bottom: 20px;
+  height: 97px;
+  overflow: auto;
 }
 
 .dialpad__table {
-    border-collapse: collapse;
-    border: 1px solid #000;
-    width: 100%;
+  border-collapse: collapse;
+  border: 1px solid #000;
+  width: 100%;
 }
 
 .dialpad__tr {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    border-bottom: 1px solid #000;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #000;
 
-    &:last-child {
-        border-bottom: none;
-    }
+  &:last-child {
+    border-bottom: none;
+  }
 }
 
 .dialpad__buttons {
-    margin-top: 30px;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
+  margin-top: 30px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
 }
 
 .dialpad__btn {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 33.333333%;
-    border-radius: 50%;
-    border: none;
-    margin-bottom: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 33.333333%;
+  border-radius: 50%;
+  border: none;
+  margin-bottom: 20px;
 }
 
 .dialpad__btn-num {
-    border-radius: 50%;
-    background-color: aquamarine;
-    width: 70px;
-    height: 70px;
-    font-size: 25px;
-    color: #000;
+  border-radius: 50%;
+  background-color: aquamarine;
+  width: 70px;
+  height: 70px;
+  font-size: 25px;
+  color: #000;
 }
 
 .dialpad__call {
-    position: absolute;
-    bottom: 180px;
-    left: 50%;
-    width: 70px;
-    height: 70px;
-    transform: translateX(-50%);
-    border-radius: 50%;
-    background: orange;
-    outline: none;
-    padding: 0;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-    transition: all 0.25s ease;
+  position: absolute;
+  bottom: 180px;
+  left: 50%;
+  width: 70px;
+  height: 70px;
+  transform: translateX(-50%);
+  border-radius: 50%;
+  background: orange;
+  outline: none;
+  padding: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  transition: all 0.25s ease;
 
-    &:hover {
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.4);
-    }
+  &:hover {
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.4);
+  }
 }
 
 .dialpad__call-icon {
-    width: 30px;
-    height: 30px;
+  width: 30px;
+  height: 30px;
 }
 </style>
